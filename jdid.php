488026fe67ff5a31d62576a1299b9ee5ed9e32a5
herrysz69 <32977466+herrysz69@users.noreply.github.com>
2018-02-27 00:05:30 +0700

    Update jdid.php

--- jdid.php
+++ jdid.php
@@ -25,13 +25,13 @@ echo"
                 ║Spam JD-ID SMS ║
              ╔══╚═══════════════╝══╗
              ║  Creator By : Herry ║
-             ╚═════════════════════╝\nInput : ";
-echo "Nomor? (ex : 8xxxx)\nInput : ";
-$Nomor = trim(fgets(STDIN));
+             ╚═════════════════════╝\n\nInput : ";
+echo "Nomor? (contoh : 8xxxx)\nInput : ";
+$nomor = trim(fgets(STDIN));
 echo "Jumlah?\nInput : ";
-$jumlah = trim(fgets(STDIN));                 
-echo "Jeda? 0-9999999999 (ex:0)\nInput : ";
+$jumlah = trim(fgets(STDIN));
+echo "Jeda? 0-99999 (ex:1)\nInput : ";
 $jeda = trim(fgets(STDIN));
-$execute = jdidbom($Nomor, $Jumlah, $jeda);
+$execute = jdidbom($nomor, $jumlah, $jeda);
 print $execute;
 ?>
