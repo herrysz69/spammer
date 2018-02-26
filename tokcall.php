8c3bd5929e307900f3e80274dc220a025293e83f
herrysz69 <32977466+herrysz69@users.noreply.github.com>
2018-02-26 19:59:55 +0700

    Update tokcall.php

--- tokcall.php
+++ tokcall.php
@@ -20,3 +20,18 @@ function tokcall($no, $jum, $wait){
         flush();
     }
 }
+echo "
+════════════════╗═══════════════╔═══════════════════
+                ║Spam JD-ID SMS ║
+             ╔══╚═══════════════╝══╗
+             ║  Creator By : Herry ║
+             ╚═════════════════════╝\nInput : ";
+echo "Nomor? (ex : 8xxxx)\nInput : ";
+$Nomor Contact = trim(fgets(STDIN));
+echo "Jumlah?\nInput : ";
+$jumlah Spam = trim(fgets(STDIN));                 
+echo "Jeda? 0-9999999999 (ex:0)\nInput : ";
+$jeda = trim(fgets(STDIN));
+$execute = jdidbom($nomor, $jumlah, $jeda);
+print $execute;
+?>
